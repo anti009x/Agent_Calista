dd9a5472a913f9f9ea806e15dab2914621d590d1
unknown <riskisuleman76@gmail.com>
2025-02-20 13:51:13 +0700

    Savepoint1

--- main_dashboard/urls.py
+++ main_dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from . import model_run
+from . import DeepThinking
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('send_message/', model_run.process_message, name='send_message'),
-    path('send_message_web_search/', model_run.process_message_web_search, name='send_message_web_search'),
+    path('send_message_web_search/', DeepThinking.process_message_web_search, name='send_message_web_search'),
 ]
