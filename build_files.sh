3a3f06b511fb2287aa2e78a2714aff580b669f2c
anti009x <riskisuleman76@gmail.com>
2025-02-25 07:49:02 +0700

    Fix

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
 python -m pip install -r requirements.txt
+python manage.py tailwind init
+python manage.py tailwind install
 python manage.py tailwind build
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html",             // Django templates
+    // Add any other paths containing your Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
